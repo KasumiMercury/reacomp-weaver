59df6220982ac97fa2a779744740b809377f5699
KasumiMercury <naoki_shira@outlook.jp>
2025-08-08 00:22:35 +0900

    feat: hono init

--- wsserver/src/index.ts
+++ wsserver/src/index.ts
@@ -1,9 +1,27 @@
-import { Hono } from 'hono'
+import {Hono} from 'hono'
+import {upgradeWebSocket} from "hono/cloudflare-workers";
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get(
+    '/signaling',
+    upgradeWebSocket((c) => {
+      return {
+        onMessage: (evt, ws) => {
+          ws.send('Received: ' + evt.data)
+        },
+        onClose: () => {
+          console.log('WebSocket connection closed')
+        },
+        onError: (err) => {
+          console.error('WebSocket error:', err)
+        }
+      }
+    })
+)
 
 export default app
